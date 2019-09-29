a0565367d43c37ca2853c8e2c590ce5168a8c5d3
Xiryl <fabio@chiarani.it>
2019-09-29 13:18:01 +0200

    set binding

--- droid/app/src/main/java/it/chiarani/diario_diabete/views/MainActivity.java
+++ droid/app/src/main/java/it/chiarani/diario_diabete/views/MainActivity.java
@@ -1,19 +1,37 @@
 package it.chiarani.diario_diabete.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import it.chiarani.diario_diabete.R;
+import it.chiarani.diario_diabete.databinding.ActivityMainBinding;
 
 import android.os.Bundle;
 
 import com.crashlytics.android.Crashlytics;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void setActivityBinding() {
+        binding = DataBindingUtil.setContentView(this, getLayoutID());
+    }
+
+    @Override
+    protected void setViewModel() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-       // Crashlytics.getInstance().crash(); // Force a crash
+        binding.test.setText("ciao");
     }
 }
