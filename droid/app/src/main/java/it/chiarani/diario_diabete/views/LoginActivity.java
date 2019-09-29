a3468ee84d772a9670455353bcb082164004a12f
Xiryl <fabio@chiarani.it>
2019-09-29 21:01:00 +0200

    add login activity

---
+++ droid/app/src/main/java/it/chiarani/diario_diabete/views/LoginActivity.java
@@ -0,0 +1,15 @@
+package it.chiarani.diario_diabete.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import it.chiarani.diario_diabete.R;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
