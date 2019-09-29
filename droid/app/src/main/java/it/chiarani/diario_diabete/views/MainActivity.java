e82c83e751b565fdd46c8e6737e459e2fc2fa3ec
Xiryl <fabio@chiarani.it>
2019-09-29 13:13:30 +0200

    add Firebase SDK

--- droid/app/src/main/java/it/chiarani/diario_diabete/views/MainActivity.java
+++ droid/app/src/main/java/it/chiarani/diario_diabete/views/MainActivity.java
@@ -5,11 +5,15 @@ import it.chiarani.diario_diabete.R;
 
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // Crashlytics.getInstance().crash(); // Force a crash
     }
 }
