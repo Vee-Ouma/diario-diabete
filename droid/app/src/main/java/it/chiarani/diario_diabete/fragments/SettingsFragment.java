13d5ed4f31431d518e40619a9526acc71b4824d6
Fabio <fabio@chiarani.it>
2019-11-08 08:07:32 +0100

    set light and dark theme for settings activity

--- droid/app/src/main/java/it/chiarani/diario_diabete/fragments/SettingsFragment.java
+++ droid/app/src/main/java/it/chiarani/diario_diabete/fragments/SettingsFragment.java
@@ -1,15 +1,19 @@
 package it.chiarani.diario_diabete.fragments;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import it.chiarani.diario_diabete.R;
 
@@ -19,15 +23,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         // Required empty public constructor
     }
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.fragment_settings, rootKey);
+
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String currentTheme = sharedPref.getString("theme", "light");
+        if (currentTheme.equals("light")) {
+            getActivity().setTheme(R.style.PreferenceScreenLight);
+        } else {
+            getActivity().setTheme(R.style.PreferenceScreenDark);
+        }
     }
 
 }
