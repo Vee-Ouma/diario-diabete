65cb3867aa4bc25b8732adb3a33bad630397089f
Fabio <fabio@chiarani.it>
2019-11-14 22:10:40 +0100

    add simple text export

--- droid/app/src/main/java/it/chiarani/diario_diabete/fragments/BottomNavigationDrawerFragment.java
+++ droid/app/src/main/java/it/chiarani/diario_diabete/fragments/BottomNavigationDrawerFragment.java
@@ -9,22 +9,36 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import it.chiarani.diario_diabete.R;
 import it.chiarani.diario_diabete.databinding.FragmentBottomNavigationDrawerBinding;
+import it.chiarani.diario_diabete.db.Injection;
+import it.chiarani.diario_diabete.db.persistence.entities.DiabeteReadingEntity;
+import it.chiarani.diario_diabete.db.persistence.entities.TagsEntity;
+import it.chiarani.diario_diabete.viewmodels.UserViewModel;
+import it.chiarani.diario_diabete.viewmodels.ViewModelFactory;
 import it.chiarani.diario_diabete.views.InfoActivity;
 import it.chiarani.diario_diabete.views.MainActivity;
 
 public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
 
     FragmentBottomNavigationDrawerBinding binding;
+    private UserViewModel mUserViewModel;
+    private final CompositeDisposable mDisposable = new CompositeDisposable();
 
     public BottomNavigationDrawerFragment() {
         // Required empty public constructor
@@ -35,6 +49,9 @@ public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bottom_navigation_drawer, container, false);
         View view = binding.getRoot();
+
+        ViewModelFactory mViewModelFactory = Injection.provideViewModelFactory(view.getContext());
+        mUserViewModel = ViewModelProviders.of(this, mViewModelFactory).get(UserViewModel.class);
 
         binding.fragmentBottomNavigationDrawerNavView.setNavigationItemSelectedListener( v-> {
             switch (v.getItemId()) {
@@ -60,6 +77,10 @@ public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
                     this.dismiss();
                     return true;
                 }
+                case R.id.bottom_menu_export_data : {
+                    runExportData();
+                    return true;
+                }
                 default: return true;
             }
 
@@ -71,6 +92,37 @@ public class BottomNavigationDrawerFragment extends BottomSheetDialogFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    private void runExportData() {
+        mDisposable.add(mUserViewModel.getUser()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe( userEntity -> {
+
+                    if (userEntity.getReadings() == null) {
+                        Toast.makeText(this.getContext(), getString(R.string.txtGenericError), Toast.LENGTH_LONG).show();
+                    }
+
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("%id;%timestamp;%date;%hour;%fasting;%eating;%value;%notes\n");
+
+                    for(DiabeteReadingEntity readingEntity : userEntity.getReadings()) {
+                        stringBuilder.append(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n", readingEntity.getId() + "", readingEntity.getTimestamp() + "", readingEntity.getReadingDate(), readingEntity.getReadingHour(), readingEntity.isFasting() + "", readingEntity.isEaten2h() + "", readingEntity.getValue() + "", readingEntity.getNotes()) );
+                    }
+
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, stringBuilder.toString());
+                    sendIntent.setType("text/plain");
+
+                    Intent shareIntent = Intent.createChooser(sendIntent, null);
+                    startActivity(shareIntent);
+
+            }, throwable -> {
+                Toast.makeText(this.getContext(), getString(R.string.txtGenericError), Toast.LENGTH_LONG).show();
+            }));
+
     }
 
 
